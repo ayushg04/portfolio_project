165f1b73ec84b75da4e7656e60b0f5e611963a92
ayushg04 <ayush.gaur@unthinkable.co>
2021-04-12 18:39:09 +0530

    Updated portfolio project with scroll view

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 
 void main() {
@@ -11,164 +13,190 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
         child: Container(
-          constraints: BoxConstraints.expand(),
           decoration: BoxDecoration(
               image: DecorationImage(
                   image: AssetImage("image/backgroud.jpeg"),
                   fit: BoxFit.cover)),
-          child: Column(
-            children: <Widget>[
-              Row(
-                children: <Widget>[
-                  //Flexible(fit: FlexFit.loose),
-                  CircleAvatar(
-                    radius: 50,
-                    backgroundImage: AssetImage("image/ayush.jpg"),
-                  ),
-                  SizedBox(
-                    width: 40,
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      //Flexible(fit: FlexFit.tight),
-                      Text(
-                        "Ayush Gaur",
-                        style: TextStyle(fontSize: 30, color: Colors.white),
-                      ),
-                      Text(
-                        "App Developer",
-                        style: TextStyle(fontSize: 17, color: Colors.white),
-                      )
-                    ],
-                  )
-                ],
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 30.0),
-                child: Column(
+          child: Padding(
+            padding: const EdgeInsets.only(left:10.0,top: 40),
+            child: Column(
+              children: <Widget>[
+                Row(
                   children: <Widget>[
-                    Row(
-                      children: <Widget>[
-                        Icon(
-                          Icons.school,
-                          size: 40,
-                          color: Colors.white,
-                        ),
-                        SizedBox(
-                          width: 27,
-                        ),
-                        //Flexible(fit: FlexFit.loose),
-                        Text(
-                          "City Montessori School, Lucknow",
-                          style: TextStyle(fontSize: 20, color: Colors.white),
-                        )
-                      ],
+                    //Flexible(fit: FlexFit.loose),
+                    CircleAvatar(
+                      radius: 70,
+                      backgroundImage: AssetImage("image/ayush.jpg"),
                     ),
                     SizedBox(
-                      height: 10,
+                      width: 30,
                     ),
-                    Row(
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
                       children: <Widget>[
-                        Icon(
-                          Icons.school,
-                          size: 40,
-                          color: Colors.white,
-                        ),
-                        SizedBox(
-                          width: 27,
+                        //Flexible(fit: FlexFit.tight),
+                        Text(
+                          "Ayush Gaur",
+                          style: TextStyle(fontSize: 33, color: Colors.white),
                         ),
                         Text(
-                          "PSIT, Kanpur",
-                          style: TextStyle(fontSize: 22, color: Colors.white),
+                          "App Developer",
+                          style: TextStyle(fontSize: 17, color: Colors.white),
                         )
                       ],
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Row(
-                      children: <Widget>[
-                        Icon(
-                          Icons.mail,
-                          size: 40,
-                          color: Colors.white,
-                        ),
-                        SizedBox(
-                          width: 27,
-                        ),
-                        Text(
-                          "ayushgaur0423@gmail.com",
-                          style: TextStyle(fontSize: 22, color: Colors.white),
-                        )
-                      ],
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Row(
-                      children: <Widget>[
-                        Icon(
-                          Icons.dialpad,
-                          size: 40,
-                          color: Colors.white,
-                        ),
-                        SizedBox(
-                          width: 27,
-                        ),
-                        Text(
-                          "888xxxxxxx",
-                          style: TextStyle(fontSize: 22, color: Colors.white),
-                        )
-                      ],
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Row(
-                      children: <Widget>[
-                        Icon(
-                          Icons.location_on,
-                          size: 40,
-                          color: Colors.white,
-                        ),
-                        SizedBox(
-                          width: 27,
-                        ),
-                        Text(
-                          "Lucknow, UP, India",
-                          style: TextStyle(fontSize: 22, color: Colors.white),
-                        )
-                      ],
-                    ),
+                    )
                   ],
                 ),
-              ),
-              SizedBox(
-                height: 40,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  "Hello, I am an iOS developer in unthinkable solutions. I have completed my schooling from CMS, Lucknow and currently I am pursuing B Tech from Psit, Kanpur. I love to play games which helps in increasing my IQ. I also love to listen soft music.",
-                  style: TextStyle(fontSize: 20, color: Colors.white),
+                SizedBox(
+                  height: 70,
                 ),
-              ),
-              SizedBox(
-                height: 80,
-              ),
-              //Flexible(fit: FlexFit.loose),
-              Text(
-                "Created by Ayush",
-                style: TextStyle(fontSize: 18, color: Colors.white),
-              ),
-            ],
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          Icon(
+                            Icons.school,
+                            size: 65,
+                            color: Colors.white,
+                          ),
+                          SizedBox(
+                            width: 30,
+                          ),
+                          //Flexible(fit: FlexFit.loose),
+                          Text(
+                            "City Montessori School, Lucknow",
+                            style: TextStyle(fontSize: 27, color: Colors.white),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Icon(
+                            Icons.school,
+                            size: 65,
+                            color: Colors.white,
+                          ),
+                          SizedBox(
+                            width: 30,
+                          ),
+                          Text(
+                            "Pranveer Singh Institute of Technology, Kanpur",
+                            style: TextStyle(fontSize: 27, color: Colors.white),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Icon(
+                            Icons.mail,
+                            size: 65,
+                            color: Colors.white,
+                          ),
+                          SizedBox(
+                            width: 30,
+                          ),
+                          Text(
+                            "ayushgaur0423@gmail.com",
+                            style: TextStyle(fontSize: 27, color: Colors.white),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Icon(
+                            Icons.phone,
+                            size: 65,
+                            color: Colors.white,
+                          ),
+                          SizedBox(
+                            width: 30,
+                          ),
+                          Text(
+                            "+91-8887418075",
+                            style: TextStyle(fontSize: 27, color: Colors.white),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Icon(
+                            Icons.location_on,
+                            size: 65,
+                            color: Colors.white,
+                          ),
+                          SizedBox(
+                            width: 30,
+                          ),
+                          Text(
+                            "Lucknow, Uttar-Pradesh, India",
+                            style: TextStyle(fontSize: 27, color: Colors.white),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 100,
+                ),
+                Text(
+                  "Hello, I am an iOS developer in unthinkable solutions. I have completed my schooling from CMS, Lucknow and currently I am pursuing B Tech from Psit, Kanpur. I love to play games which helps in increasing my IQ. I also love to listen soft music.",
+                  style: TextStyle(fontSize: 27, color: Colors.white),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(
+                  height: 100,
+                ),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Column(
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          Icon(
+                            Icons.event_available,
+                            size: 130,
+                            color: Colors.white,
+                          ),
+
+
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+                //Flexible(fit: FlexFit.loose),
+                Text(
+                  "PAS Project - Worked on different pages to make them updated with javascript, HTML, CSS.",
+                  style: TextStyle(fontSize: 30, color: Colors.white),
+                ),
+                SizedBox(
+                  height: 100,
+                ),
+                Text(
+                  "Created by Ayush",
+                  style: TextStyle(fontSize: 18, color: Colors.white),
+                ),
+              ],
+            ),
           ),
         ),
       ),
